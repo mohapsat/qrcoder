42d75f3e3db3093a1be8f7d9322c421552f13405
Satya Mohapatra <mohapsat@gmail.com>
2017-08-10 15:59:19 -0700

    qr batch form not working

--- application/form.py
+++ application/form.py
@@ -6,3 +6,6 @@ from wtforms.validators import DataRequired
 class QRForm(FlaskForm):
     qrtext = StringField('Enter text or URL', validators=[DataRequired()])
 
+
+class QRBatchForm(FlaskForm):
+    qr_csvfileinput = StringField('File Input')
--- application/views.py
+++ application/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, json
 from application import app
 # imports app variable from application package
-from .form import QRForm
-from qrcode_generator import generate_qrcode
+from .form import QRForm, QRBatchForm
+from qrcode_generator import generate_qrcode, generate_qrcode_batch_csv
 
 
 # @app.route('/')
@@ -10,6 +10,7 @@ from qrcode_generator import generate_qrcode
 def index():
 
     form = QRForm()
+    batchform = QRBatchForm()
 
     if form.validate_on_submit():
         responseURL = generate_qrcode(form.qrtext.data)
@@ -23,6 +24,22 @@ def index():
                                responseURL=responseURL
                                )
 
+    if batchform.validate_on_submit():
+        inputFile = 'qr_fileinput.csv'
+        app.logger.info(inputFile)
+        qrBatchJsonResponse = generate_qrcode_batch_csv(inputFile)
+        app.logger.info(qrBatchJsonResponse)
+        flash('You requested Batch QR codes for: "%s"' % inputFile)
+        # app.logger.info(form.qrtext.data)
+        # app.logger.info(responseURL)
+
+        # return redirect('/index')
+        return render_template('index.html',
+                               batchform=batchform,
+                               qrBatchJsonResponse=qrBatchJsonResponse
+                               )
+
     return render_template('index.html',
                            form=form
+                           # batchform=batchform
                            )
